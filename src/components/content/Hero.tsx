5a9ddf5737f77e46541b0d745e1024bd98800b4b
Vivek <vivekcse.in@gmail.com>
2024-01-27 02:41:24 +0530

    tricolor background gradient background updated

--- src/components/content/Hero.tsx
+++ src/components/content/Hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../../styles/content/Hero.scss";
 const Hero = () => {
   return (
-    <div className="fullscreen Hero">
+    <div className="fullscreen bg_tricolor Hero">
       A NextJS starter template to create full-stack production level
       web-applications.
     </div>
